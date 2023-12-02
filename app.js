68b197c937e479e12943ad8d4b63fc3b0e4ec20d
Satoshi <uglysatoshi@ya.ru>
2023-12-03 02:31:52 +0300

    added logic for figures movements and some styles for background of the board

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const gameBoard = document.querySelector('#board');
 const activePlayer = document.querySelector('#player');
 const infoDisplay = document.querySelector('#information');
+
 const width = 8;
+let playerGo = 'white';
+activePlayer.textContent = 'white';
+let startPosition;
+let draggedFigure;
 
 const startFigurePosition = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -37,5 +42,79 @@ function createBoard() {
         gameBoard.append(square);
     })
 }
-
 createBoard()
+const allSquares = document.querySelectorAll(".square")
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart);
+    square.addEventListener('dragover', dragOver);
+    square.addEventListener('drop', dragDrop)
+})
+
+function dragStart (event) {
+    startPosition = event.target.parentNode.getAttribute('square-id');
+    draggedFigure = event.target;
+}
+
+function dragOver (event) {
+    event.preventDefault();
+}
+
+
+function dragDrop (event) {
+    event.stopPropagation();
+    const correctTarget = draggedFigure.classList.contains(playerGo);
+    console.log(draggedFigure.classList.contains(playerGo));
+    const taken = event.target.classList.contains('figure');
+    const opponentTarget = playerGo === 'white' ? 'black' : 'white';
+    const valid = validateMove(draggedFigure);
+    const takenByOpponent = event.target?.classList.contains(opponentTarget);
+
+    if(correctTarget) {
+        if(takenByOpponent && valid) {
+            event.target.parentNode.append(draggedFigure);
+            event.target.remove();
+            changePlayer();
+            return;
+        }
+        if(taken && !takenByOpponent) {
+            infoDisplay.textContent = "This move isn't valid!";
+            setTimeout(() => infoDisplay.textContent = "", 2000);
+            return;
+        }
+        if(valid) {
+            event.target.append(draggedFigure);
+            changePlayer();
+        }
+    }
+}
+
+function changePlayer() {
+    if (playerGo === 'black') {
+        reverseIDs();
+        playerGo = 'white';
+        activePlayer.textContent = 'white';
+    } else {
+        revertIDs();
+        playerGo = 'black';
+        activePlayer.textContent = 'black';
+    }
+}
+
+function reverseIDs() {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) =>
+        square.setAttribute('square-id', (width * width - 1) - i)
+    );
+}
+
+function revertIDs() {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) =>
+        square.setAttribute('square-id',  i)
+    );
+}
+
+function validateMove(target) {
+    return true;
+}
